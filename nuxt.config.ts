289445f3483f5abe47f13ae9c0978e28a404ce51
LumiTrend <lumitrend@volcmail.com>
2025-04-13 19:36:47 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,16 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   extends: ['shadcn-docs-nuxt'],
+  content: {
+    documentDriven: true,
+    highlight: {
+      theme: {
+        // Default theme
+        default: 'github-light',
+        // Theme used in dark-mode
+        dark: 'github-dark',
+      }
+    },
+  },
   compatibilityDate: '2024-07-06',
 });
