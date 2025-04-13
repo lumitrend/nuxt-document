8c48a2d9fa16d63dceb6565095b69ec8fe26d20a
LumiTrend <lumitrend@volcmail.com>
2025-04-13 22:08:06 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,21 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   extends: ['shadcn-docs-nuxt'],
+  i18n: {
+    defaultLocale: 'cn',
+    locales: [
+      {
+        code: 'cn',
+        name: '简体中文',
+        language: 'zh-CN',
+      },
+      {
+        code: 'en', // language 2 letters code (e.g. 'fr')
+        name: 'English', // language name (e.g. 'Français')
+        language: 'en-US', // language ISO code (e.g. 'fr-FR')
+      },
+    ],
+  },
   content: {
     documentDriven: true,
     highlight: {
