0d7773cdc4e202d90d1b094de16c3107e702ce85
LumiTrend <lumitrend@volcmail.com>
2025-04-14 03:29:40 +0800

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default defineAppConfig({
       name: 'Vaeluon',
       description: 'Beautifully designed Nuxt Content template built with shadcn-vue. Customizable. Compatible. Open Source.',
       ogImage: '/assets/og.png',
-     / ogImageComponent: 'BlogPost',
+      ogImageComponent: 'BlogPost',
       ogImageColor: 'light',
     },
     theme: {
